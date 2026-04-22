851af72f854aa7c92d28bc74f7fd3f3592a1a32b
Aly-exe <alibnraslan@gmail.com>
2026-04-23 00:46:03 +0200

    reclean code

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:notes_app/core/colors.dart';
 import 'package:notes_app/view/widgets/custom_appbar.dart';
-import 'package:notes_app/view/widgets/custom_text_button.dart';
-import 'package:notes_app/view/widgets/custom_text_form_field.dart';
+import 'package:notes_app/view/widgets/custom_floatingAction_button.dart';
 import 'package:notes_app/view/widgets/notes_view.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -25,61 +22,6 @@ class HomeScreen extends StatelessWidget {
           ),
         ) ,
         floatingActionButton: CustomFloatingActionBottom(),
-      ),
-    );
-  }
-}
-
-class CustomFloatingActionBottom extends StatelessWidget {
-  const CustomFloatingActionBottom({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(decoration: BoxDecoration(
-      color: kSecondaryColor ,
-      borderRadius: BorderRadius.circular(36)
-    ) , child: IconButton(onPressed: (){
-      showModalBottomSheet(context: context, builder: (context){
-        return BottomSheetForm();
-      });
-    }, icon: FaIcon(FontAwesomeIcons.add , color: Colors.black,)));
-  }
-}
-
-class BottomSheetForm extends StatefulWidget {
-  const BottomSheetForm({
-    super.key,
-  });
-
-  @override
-  State<BottomSheetForm> createState() => _BottomSheetFormState();
-}
-
-class _BottomSheetFormState extends State<BottomSheetForm> {
-  final  GlobalKey<FormState> formkey =GlobalKey();
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(16.0),
-      child: Form(
-        key:formkey,
-        child: Column(
-          children: [
-            CustomTextFormField(
-              hintText: "title",
-            ),
-            SizedBox(height: 10,),
-            CustomTextFormField(
-              hintText: "content",
-              maxLines: 4,
-            ),
-            Spacer(),
-            CustomTextButton()
-          ],
-        ),
       ),
     );
   }
