1192147c002adba55d1bb1f5e0d4d972e4a85d8b
Aly-exe <alibnraslan@gmail.com>
2026-04-23 23:43:01 +0200

    add extends HiveObject to model to help me to delete note easily

--- lib/view/widgets/note_box.dart
+++ lib/view/widgets/note_box.dart
@@ -1,17 +1,25 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:notes_app/core/colors.dart';
+import 'package:notes_app/cubit/cubit/note_cubit.dart';
+import 'package:notes_app/models/note_model.dart';
 
 class NoteBox extends StatelessWidget {
   const NoteBox({
     super.key,
+    required this.title,
+    required this.subtitle,
+    required this.date,
+    required this.color, required this.note,
   });
-
+  final String title, subtitle, date;
+  final int color;
+  final NoteModel note;
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(16.0),
       decoration: BoxDecoration(
         color: kPrimaryColor,
         borderRadius: BorderRadius.circular(16),
@@ -19,31 +27,50 @@ class NoteBox extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
-           ListTile(
+          ListTile(
             title: Padding(
               padding: const EdgeInsets.only(bottom: 16),
-              child: Text("Flutter tips" , style: TextStyle(
-                color: Colors.black ,
-                fontSize: 26,
-              ),),
+              child: Text(
+                title,
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
             ),
-            subtitle: Padding(            
+            subtitle: Padding(
               padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Text("Build Your Career with Aly Raslan" , style: TextStyle(
-                fontSize: 18 ,
-                color: Color.fromARGB(255, 112, 111, 73)
-              ),),
+              child: Text(
+                subtitle,
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Color.fromARGB(255, 112, 111, 73),
+                ),
+              ),
             ),
-            trailing: IconButton(onPressed: (){}, icon: FaIcon(FontAwesomeIcons.trash , color: Colors.black, size: 30,)),
+            trailing: BlocBuilder<NoteCubit, NoteState>(
+              builder: (context, state) {
+                return IconButton(
+                  onPressed: () async{
+                    await note.delete();
+                  },
+                  icon: FaIcon(
+                    FontAwesomeIcons.trash,
+                    color: Colors.black,
+                    size: 30,
+                  ),
+                );
+              },
+            ),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              Text("April 21 , 2026" , style: TextStyle(color:Color.fromARGB(255, 112, 111, 73)),),
+              Text(
+                date,
+                style: TextStyle(color: Color.fromARGB(255, 112, 111, 73)),
+              ),
             ],
-          )
+          ),
         ],
-      )
+      ),
     );
   }
 }
